d5d9ab9d5680c79a81c822e0cec4d0c3a39539bc
xhsj8125 <1510332604@qq.com>
2021-02-10 19:48:33 +0800

    257

--- 257.binary-tree-paths.c
+++ 257.binary-tree-paths.c
@@ -47,10 +47,43 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void backtrace(struct TreeNode* node, char **res, )
+#define PATH_MAX_NUM    100000
+void backtrace(struct TreeNode *root, int *nums, int n, char **returnArray, int *returnSize)
+{
+    if (root->left) {
+        nums[n++] = root->val;
+        backtrace(root->left, nums, n, returnArray, returnSize);
+        n--;
+    }
+    if (root->right) {
+        nums[n++] = root->val;
+        backtrace(root->right, nums, n, returnArray, returnSize);
+        n--;
+    }
+    if (!root->left && !root->right){
+        int i;
+        nums[n++] = root->val;
+        returnArray[*returnSize] = malloc(sizeof(char) * n * 11);
+        char *tmp = returnArray[*returnSize];
+        for (i = 0; i < n - 1; i++) {
+            tmp += sprintf(tmp, "%d->", nums[i]);
+        }
+        sprintf(tmp, "%d", nums[i]);
+        (*returnSize)++;
+    }
+}
 
 char ** binaryTreePaths(struct TreeNode* root, int* returnSize){
+    char **returnArray;
+    int nums[1000];
+    int n = 0;
 
+    *returnSize = 0;
+    if (root == NULL)
+        return NULL;
+    returnArray = (char **)malloc(sizeof(char *) * PATH_MAX_NUM);
+    backtrace(root, nums, n, returnArray, returnSize);
+    return returnArray;
 }
 
 
